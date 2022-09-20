a4f4bd1607d269d2f5c2fe9180687610fc62aa11
Gabriela Sanches <112426102+snchs-gb@users.noreply.github.com>
2022-09-20 11:14:21 -0300

    Update muito_simples_p1.c

--- muito_simples_p1.c
+++ muito_simples_p1.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 
-void distancia(float x1, float y1, float x2, float y2){
+void distancia(float x1, float y1, float x2, float y2){ //recebe as coordenadas de dois pontos
 
     float distancia;
 
-    distancia = sqrt(pow(x2-x1, 2) + pow(y2-y1, 2));
+    distancia = sqrt(pow(x2-x1, 2) + pow(y2-y1, 2)); //distancia entre dois pontos = √((x2-x1)^2+(y2-y1)^2)
 
     printf("%.5f", distancia);
 
@@ -14,7 +14,7 @@ void distancia(float x1, float y1, float x2, float y2){
 
 int main (void){
 
-    distancia(1, 2, 5, 6);
+    distancia(1, 2, 5, 6); //exemplo de chamada da função 
 
     return 0;
 }
