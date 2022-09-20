31480c7695b2897290fceaa448f07650d981a24c
Gabriela Sanches <112426102+snchs-gb@users.noreply.github.com>
2022-09-20 09:29:59 -0300

    Update simples_p1.c

--- simples_p1.c
+++ simples_p1.c
@@ -8,17 +8,17 @@ int main (void){
     int x, y;
     int soma = 0;
 
-    for (i=0; i<10 ;i++){
+    for (i=0; i<10 ;i++){ //le vetor de 10 posições
         
         scanf("%i", &vetor[i]);
     }
 
     printf("Digite x: ");
-    scanf("%i", &x);
+    scanf("%i", &x); //le a primeira posição
     printf("Digite y: ");
-    scanf("%i", &y);
+    scanf("%i", &y); //le a segunda posição
 
-    soma = vetor[x-1]+vetor[y-1];
+    soma = vetor[x-1]+vetor[y-1]; //soma é a soma da posição x + y (respeitando indices)
 
     printf("Soma = %i", soma);
 
