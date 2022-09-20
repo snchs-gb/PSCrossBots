889b6e0a48ea122e94c0f6150d149af63da2d74c
Gabriela Sanches <112426102+snchs-gb@users.noreply.github.com>
2022-09-20 09:41:14 -0300

    Update simples_p4.c

--- simples_p4.c
+++ simples_p4.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-void encontra_letra(char letra, const char* string){
+void encontra_letra(char letra, const char* string){ //recebe uma string e um caracter(diferencia maiúscula de minúscula)
     
     int i;
     int contador = 0;
 
-    for(i=0;i<strlen(string);i++){
-        if(string[i]==letra){
+    for(i=0;i<strlen(string);i++){  //i de 0 até menor que o tamanho da string
+        if(string[i]==letra){ //se um elemento da string for igual ao caracter informado, contador soma 1
             contador++;
         }
     }
 
-    printf("%i", contador);
+    printf("%i", contador); //ao final imprime o contador
 }
 
 int main (void){
 
-    encontra_letra('a',"uma frase generica de questões de programação");
+    encontra_letra('a',"uma frase generica de questões de programação"); //exemplo de chamada
 
     return 0;
 }
